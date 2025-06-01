f0afa3263dec4375a2c76f3e8d7606ab1b8e878a
bahrulrozak <bahrulrozak02@gmail.com>
2025-06-01 09:27:34 +0700

    feat (chat app) : config awal na

---
+++ Chat-App/config.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";        // sesuaikan dengan user MySQL lo
+$password = "";        // sesuaikan password MySQL lo
+$dbname = "ratna_chat_app";
+
+// Buat koneksi
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal sayang: " . $conn->connect_error);
+}
+?>
